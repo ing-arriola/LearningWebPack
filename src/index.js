8e8eab7df0c93fb959656fe6a070f4a044c1e7d5
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-17 18:36:32 -0600

    Babel added to the project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 const products=["product1","product2","product3"]
 
 console.log(products)
+
+const customer=(name) => customerName
+
+class CustomerUser{
+    constructor(name){
+        this.name=name
+    }
+}
+
+let customerOp=new CustomerUser(`Jaime Arriola`)
+
+console.log(customerOp)
