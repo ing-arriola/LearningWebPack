8bb36a0cc039b7d667a44dac56bfd6cdf8cf7c7f
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-18 18:57:54 -0600

    Added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,12 @@ module.exports={
         filename:'[name].bundle.js',
         path:path.join(__dirname,'/dist')
     },
+    devServer:{
+        contentBase:path.join(__dirname,'dist'),
+        compress:true,
+        port:9000
+    }
+    ,
     module:{
         rules:[
             {
