8299d5c51eaefff00e39ae939cddd56b2308d124
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-18 18:17:53 -0600

    Create multiple bundles with multiple entry points

---
+++ src/js/company.js
@@ -0,0 +1,17 @@
+import '../css/style.scss'
+
+const products=["product1","product2","product3"]
+
+console.log(products)
+
+const customer=(name) => customerName
+
+class Company{
+    constructor(name){
+        this.name=name
+    }
+}
+
+let company=new Company(`Company 01`)
+
+console.log(company)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path=require('path')
 
 module.exports={
-    entry:'./src/js/index.js',
+    entry:{
+        index: './src/js/index.js',
+        company:'./src/js/company.js'
+    },
     output:{
-        filename:'bundle.js',
+        filename:'[name].bundle.js',
         path:path.join(__dirname,'/dist')
     },
     module:{
